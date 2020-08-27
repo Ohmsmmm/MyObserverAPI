21374c7cf14d835ebf9978e202a2f095e1c0aba7
jirapon jermkure <Ohmsm0150@gmail.com>
2020-08-27 10:08:46 +0700

    fix observer

---
+++ observer/Senior.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+import java.util.Observer;
+import java.util.Observable;
+/**
+ *
+ * @author sarun
+ */
+public class Senior implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println(
+                "(senior) headquater is updated to " +
+                        ((HeadQuater)o).getSomeData());
+    }
+}
